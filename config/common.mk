fc8b928418b3cf81f603cc6c1dadec4b813523d1
SenseiDEVLinux <klimashevskiymaxim2003@gmail.com>
2025-03-31 11:31:46 +0300

    Drop microg

--- config/common.mk
+++ config/common.mk
@@ -182,11 +182,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 ifeq ($(TARGET_BUILD_GAPPS),true)
     $(call inherit-product-if-exists, vendor/google/gms/config.mk)
-else
-    $(call inherit-product, external/svox/svox_tts.mk)
-    $(call inherit-product, vendor/microg/products/gms.mk)
+    $(call inherit-product-if-exists, vendor/google/pixel/config.mk)
 endif
-$(call inherit-product-if-exists, vendor/google/pixel/config.mk)
 
 #OTA tools
 PRODUCT_HOST_PACKAGES += \
